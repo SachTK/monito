62b4f704b09a1c8d59ccd91bbc26da0750558f5e
Sachini Tharuka <sachinitkandambige29@gmail.com>
2024-10-19 21:01:37 +0530

    add axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,43 +1,31 @@
+import axios from 'axios';
+
 export const fetchPuppies = async () => {
-    try {
-      const response = await fetch('https://monitor-backend-rust.vercel.app/api/pets');
-      if (!response.ok) {
-        throw new Error('Failed to fetch puppies');
-      }
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error('Error fetching puppy data:', error);
-      throw error; 
-    }
-  };
-  
+  try {
+    const response = await axios.get('https://monitor-backend-rust.vercel.app/api/pets');
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching puppy data:', error);
+    throw error; 
+  }
+};
 
-  export const fetchProducts = async () => {
-    try {
-      const response = await fetch('https://monitor-backend-rust.vercel.app/api/products');
-      if (!response.ok) {
-        throw new Error('Failed to fetch products');
-      }
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error('Error fetching product data:', error);
-      throw error; 
-    }
-  };
+export const fetchProducts = async () => {
+  try {
+    const response = await axios.get('https://monitor-backend-rust.vercel.app/api/products');
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching product data:', error);
+    throw error; 
+  }
+};
 
-  export const fetchCustomers = async () => {
-    try {
-      const response = await fetch('https://monitor-backend-rust.vercel.app/api/customers');
-      if (!response.ok) {
-        throw new Error('Failed to fetch customers');
-      }
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error('Error fetching customers data:', error);
-      throw error; 
-    }
-  };
-  
+export const fetchCustomers = async () => {
+  try {
+    const response = await axios.get('https://monitor-backend-rust.vercel.app/api/customers');
+    return response.data; 
+  } catch (error) {
+    console.error('Error fetching customers data:', error);
+    throw error; 
+  }
+};
